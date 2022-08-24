74fac7d3ee3775a8284b7ab68bb6f88805b541f1
Jayden Cho - MacBook Pro <kyutcho@umich.edu>
2022-08-23 18:35:40 -0700

    testing fastapi

--- starter/main.py
+++ starter/main.py
@@ -1,7 +1,18 @@
+from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class TaggedItem(BaseModel):
+    name: str
+    tags: Union[str, list]
+    item_id: int
+    
 @app.get("/")
 def home():
     return {"Data": "Test"}
+
+@app.post("/items/")
+def create_item(item: TaggedItem):
+    return item
